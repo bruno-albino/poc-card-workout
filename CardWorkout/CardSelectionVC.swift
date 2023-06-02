2557e2a646dd8d231f0a3e92b8ce12ac428d2745
Bruno Albino <bruno-albino@hotmail.com>
2023-06-01 21:18:40 -0300

    add timer and start/stop depending on which button tapped

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-
+    @IBOutlet var cardImageView: UIImageView!
+    var started = false
+    var timer: Timer!
+    var cards: [UIImage] = Card.allCards
+    @IBOutlet var ctaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
     }
-    */
+    
+    func start() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(switchCard), userInfo: nil, repeats: true)
+        started = true
+        ctaButton.setTitle("Stop", for: .normal)
+        ctaButton.backgroundColor = .systemRed
+    }
+    
+    func stop() {
+        timer.invalidate()
+        started = false
+        ctaButton.setTitle("Start", for: .normal)
+        ctaButton.backgroundColor = .systemGreen
+    }
+    
+    @objc func switchCard() {
+        cardImageView.image = cards.randomElement()
+    }
 
+    
+    @IBAction func ctaButtonTapped(_ sender: Any) {
+        if !started {
+            return start()
+        }
+        
+        return stop()
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+        if started {
+            return stop()
+        }
+    }
 }
